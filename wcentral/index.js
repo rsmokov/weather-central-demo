e9feba37c021e89013de855ef310844214b56386
Radoslav Smokov <rsmokov@gmail.com>
2018-04-05 11:51:46 +0300

    websocket server added

--- wcentral/index.js
+++ wcentral/index.js
@@ -32,24 +32,24 @@ function WCentralServer() {
 
         // predef db operations
         let timestamp = [];/* update timestamp */
-        const updatetime = 10, /* update timeSpan in sec */            
-            insertUnit = function(id) {
-                db.run("INSERT INTO ws_unit (id, loc_name) VALUES ("+id+", NULL)")
+        const updatetime = 10, /* update timeSpan in sec */
+            insertUnit = function (id) {
+                db.run("INSERT INTO ws_unit (id, loc_name) VALUES (" + id + ", NULL)")
             },
-            insertData = function(data) {
+            insertData = function (data) {
                 const values = [data.id, data.temp, data.hum];
-                db.run("INSERT INTO ws_data (unit_id, temp, hum, record_time) VALUES ("+values.toString()+",  datetime())");
+                db.run("INSERT INTO ws_data (unit_id, temp, hum, record_time) VALUES (" + values.toString() + ",  datetime())");
             };
 
         //db on init
-        db.serialize(function() {
+        db.serialize(function () {
             db.run("CREATE TABLE IF NOT EXISTS ws_unit (id INTEGER PRIMARY KEY, loc_name VARCHAR)");
             db.run("CREATE TABLE IF NOT EXISTS ws_data (id INTEGER PRIMARY KEY AUTOINCREMENT, unit_id INTEGER, temp INTEGER, hum INTEGER, record_time TEXT)");
-        });   
+        });
 
         // Client App on
         app.use(express.static(path.join(__dirname, 'wclient', 'dist')));
-        app.set('views', __dirname );
+        app.set('views', __dirname);
         app.set('view engine', 'html');
 
         app.get('/', function (req, res) {
@@ -57,136 +57,212 @@ function WCentralServer() {
         });
 
         // API REQUESTS
-        app.get('/allstations', function(req, res){
+        app.get('/allstations', function (req, res) {
             db.all(`SELECT * FROM ws_unit`,
-             function(err, rows){
-                if(!err && rows){
-                     res.json(rows);
+                function (err, rows) {
+                    if (!err && rows) {
+                        res.json(rows);
                     }
-                else res.json(null);
-            });
+                    else res.json(null);
+                });
         });
-        app.get('/wsdata/:id', function(req, res){
+        app.get('/wsdata/:id', function (req, res) {
             db.all(`SELECT temp, hum, record_time FROM ws_data WHERE unit_id LIKE ${req.params.id} AND record_time >= date('now', '-1 days')`,
-             function(err, rows){
-                if(!err && rows){
-                     res.json(rows);
+                function (err, rows) {
+                    if (!err && rows) {
+                        res.json(rows);
                     }
-                else res.json(null);
-            });
-        }); 
-        app.get('/wsunit/:id', function(req, res){
+                    else res.json(null);
+                });
+        });
+        app.get('/wsunit/:id', function (req, res) {
             db.get(`SELECT * FROM ws_unit WHERE id LIKE ${req.params.id}`,
-             function(err, row){
-                 if(!err && row){
-                     res.json(row);
+                function (err, row) {
+                    if (!err && row) {
+                        res.json(row);
                     }
-                else res.json(null); 
-            });
-        }); 
-        app.options('/changename/:id', function(req, res){
+                    else res.json(null);
+                });
+        });
+        app.options('/changename/:id', function (req, res) {
             res.status(200).end();
         });
-        app.put('/changename/:id', function(req, res){
-            if(req && req.body && req.body.loc_name){
+        app.put('/changename/:id', function (req, res) {
+            if (req && req.body && req.body.loc_name) {
                 const postdata = [req.body.loc_name, req.params.id];
                 const sql = `UPDATE ws_unit
                             SET loc_name = ?
                             WHERE id = ?`;
-                
-                db.run(sql, postdata, function(err, row){
-                    if(!err && row){
+
+                db.run(sql, postdata, function (err, row) {
+                    if (!err && row) {
                         res.json("updated");
-                        }
+                    }
                     else res.json(err);
                 });
-             }
-            else res.json("noting to update"); 
+            }
+            else res.json("noting to update");
         });
 
         // Handle 404 - Keep this as a last route
         app.use(function (req, res, next) {
-             res.status(404).json({ error: 'Not existing.' });
-         }); 
+            res.status(404).json({ error: 'Not existing.' });
+        });
+
+        // Setup Websocket
+        const ws = require("nodejs-websocket");
+        let cl = [];        /* the socketIo client holder */
+        let sensors = [];
+        let wsTimeout = null;
+        let wsInterval = null;
+
+        function startWsTimeout() {
+            wsTimeout = 0;
+            wsInterval = setInterval(function(){
+                if(wsTimeout !== null && wsTimeout < 4000){
+                    wsTimeout += 1000;
+                }
+                else {
+                    clearInterval(wsInterval);
+                    wsTimeout = null;
+                    let sensors = [];
+                    cl[0].broadcast.emit('broadClient', cleanArray(sensors));
+                }
+            }, 1000)
+        }
+
+        function cleanArray(actual) {
+            var newArray = new Array();
+            for (var i = 0; i < actual.length; i++) {
+              if (actual[i]) {
+                newArray.push(actual[i]);
+              }
+            }
+            return newArray;
+          }
+
+        const WSserver = ws.createServer(function (conn) {
+
+            console.log("New websocket connection, SENSOR")
+            const data = {
+                id: null,
+                status: 1,
+                temp: null,
+                hum: null
+            }
+
+            if (conn.headers['device'] === 'weatherSensor') {
+                conn.sendText('sensorReady');
+            }
+            
+            conn.on("text", function (str) {       
+                clearInterval(wsInterval);
+                str = JSON.parse(str);
+                if (conn.OPEN) {
+                    data['id'] = parseInt(str['id']);
+                    data['temp'] = parseInt(str['temp']).toFixed(2);
+                    data['hum'] = parseInt(str['hum']).toFixed(2);
+
+                    sensors[data['id']] = data;
+
+                    console.log(cleanArray(sensors), 'sensors');
+                    
+                    if (cl.length > 0) {
+                       // console.log(sensors, "sensors");
+                        //   dbRecord(sensors[str['id']]); 
+                        // only one client have to broadcast
+                        cl[0].broadcast.emit('broadClient', cleanArray(sensors));
+                        startWsTimeout();
+                    }
+                }
+            });
+
+            conn.on("close", function (code, reason) {
+                console.log("Websocket Connection closed")
+                sensors = [];
+                cl[0].broadcast.emit('broadClient', cleanArray(sensors));
+            })
+        }).listen(3001, "192.168.200.49");
 
         // Initialize the socket server
         const io = socket(server);
         io.on('connection', function (client) {
             console.log(`Client ${client.id} connected`);
-
+            //add new client to clients holder
+            cl.push(client);
             // receiving data from weather stations and broadcasting it back
             client.on('wsEmitDB', function (data) {
                 // prevent to often writing io to DB 
-                if ( Math.floor((new Date() - timestamp[data.id])/6000) > updatetime ) {
-                    timestamp[data.id] =  new Date();      
-                    if(data && data.status === 1){
-                         insertData(data);
+                if (Math.floor((new Date() - timestamp[data.id]) / 6000) > updatetime) {
+                    timestamp[data.id] = new Date();
+                    if (data && data.status === 1) {
+                        insertData(data);
                     }
-                }                     
-            }); 
+                }
+            });
             client.on('wsCreated', function (id) {
-                db.get("SELECT * FROM ws_unit WHERE id LIKE " + id, function(err, row){
-                    if(err){
+                db.get("SELECT * FROM ws_unit WHERE id LIKE " + id, function (err, row) {
+                    if (err) {
                         console.log(err, "err db")
                     }
-                    else{
-                        if(!row) insertUnit(id);
+                    else {
+                        if (!row) insertUnit(id);
                         //init timestamp for the db record
                         timestamp[id] = 0;
-                        }
-                });  
-            }); 
-            client.on('disconnect', function (data) {                
-                console.log("Client "+client.id +" disconected");
-            });     
-            client.on('connect', function (data) {                
-                console.log("Client "+client.id +" conected");                
-            });    
+                    }
+                });
+            });
+            client.on('disconnect', function (data) {
+                console.log("Client " + client.id + " disconected");
+            });
+            client.on('connect', function (data) {
+                console.log("Client " + client.id + " conected");
+            });
             // Event msg to the control center
             client.on('wsEmitViewr', function (data) {
                 client.broadcast.emit('broadClient', data);
-            }); 
-            client.on('wsRespondAlloff', function(){
+            });
+            client.on('wsRespondAlloff', function () {
                 io.sockets.clients().emit('respondAlloff');
-            });        
+            });
 
             client.on('clearall', function (id) {
                 let msg = [];
-                db.all("DELETE FROM ws_unit", function(err, row){
-                    if(err){
+                db.all("DELETE FROM ws_unit", function (err, row) {
+                    if (err) {
                         console.log(err, "err db")
                     }
-                    else{
+                    else {
                         msg.push("All units deleted");
                         io.sockets.clients().emit('dbDeleteResp', msg);
                     }
-                }); 
-                db.all("DELETE FROM ws_data", function(err, row){
-                    if(err){
+                });
+                db.all("DELETE FROM ws_data", function (err, row) {
+                    if (err) {
                         console.log(err, "err db")
                     }
-                    else{
+                    else {
                         msg.push("All data fot the units deleted");
                         io.sockets.clients().emit('dbDeleteResp', msg);
                     }
-                }); 
+                });
             });
             // Event msgs to the weather stations
-            client.on('turnoff', function(id){
+            client.on('turnoff', function (id) {
                 io.sockets.clients().emit('wsturnoff', id);
             });
-            client.on('backon', function(id){
+            client.on('backon', function (id) {
                 io.sockets.clients().emit('wsbackon', id);
             });
-            client.on('maintance', function(id){
+            client.on('maintance', function (id) {
                 io.sockets.clients().emit('wsmaintance', id);
             });
-            client.on('alloff', function(){
+            client.on('alloff', function () {
                 io.sockets.clients().emit('wsalloff');
                 io.sockets.clients().emit('clientAlloff');
             });
         });
-        
+
         console.log('Wheater central started.');
 
         open('http://localhost:' + port);
